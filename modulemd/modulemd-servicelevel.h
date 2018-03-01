7dd91c0060e18e5ba33862c0eeaca041f87c5757
Stephen Gallagher <sgallagh@redhat.com>
2018-02-28 21:40:00 -0500

    Add dup() and copy() for ServiceLevel
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/modulemd-servicelevel.h
+++ modulemd/modulemd-servicelevel.h
@@ -46,6 +46,9 @@ modulemd_servicelevel_get_eol (ModulemdServiceLevel *self);
 const GDate *
 modulemd_servicelevel_peek_eol (ModulemdServiceLevel *self);
 
+GDate *
+modulemd_servicelevel_dup_eol (ModulemdServiceLevel *self);
+
 void
 modulemd_servicelevel_set_name (ModulemdServiceLevel *self, const gchar *name);
 
@@ -54,6 +57,12 @@ modulemd_servicelevel_get_name (ModulemdServiceLevel *self);
 
 const gchar *
 modulemd_servicelevel_peek_name (ModulemdServiceLevel *self);
+
+gchar *
+modulemd_servicelevel_dup_name (ModulemdServiceLevel *self);
+
+ModulemdServiceLevel *
+modulemd_servicelevel_copy (ModulemdServiceLevel *self);
 
 G_END_DECLS
 

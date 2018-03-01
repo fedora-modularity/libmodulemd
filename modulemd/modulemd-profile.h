830400c5c1176f7ae4d652a6bf124fb2281f26fe
Stephen Gallagher <sgallagh@redhat.com>
2018-02-28 21:30:03 -0500

    Add dup() and copy() for Profile
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/modulemd-profile.h
+++ modulemd/modulemd-profile.h
@@ -45,6 +45,8 @@ modulemd_profile_get_description (ModulemdProfile *self);
 
 const gchar *
 modulemd_profile_peek_description (ModulemdProfile *self);
+gchar *
+modulemd_profile_dup_description (ModulemdProfile *self);
 
 void
 modulemd_profile_set_name (ModulemdProfile *self, const gchar *name);
@@ -52,6 +54,8 @@ const gchar *
 modulemd_profile_get_name (ModulemdProfile *self);
 const gchar *
 modulemd_profile_peek_name (ModulemdProfile *self);
+gchar *
+modulemd_profile_dup_name (ModulemdProfile *self);
 
 void
 modulemd_profile_set_rpms (ModulemdProfile *self, ModulemdSimpleSet *rpms);
@@ -61,12 +65,17 @@ modulemd_profile_get_rpms (ModulemdProfile *self);
 
 ModulemdSimpleSet *
 modulemd_profile_peek_rpms (ModulemdProfile *self);
+ModulemdSimpleSet *
+modulemd_profile_dup_rpms (ModulemdProfile *self);
 
 void
 modulemd_profile_add_rpm (ModulemdProfile *self, const gchar *rpm);
 
 void
 modulemd_profile_remove_rpm (ModulemdProfile *self, const gchar *rpm);
+
+ModulemdProfile *
+modulemd_profile_copy (ModulemdProfile *self);
 
 G_END_DECLS
 

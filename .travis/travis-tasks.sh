99b2072bfb7017d32922a719003d7c807737ace0
Stephen Gallagher <sgallagh@redhat.com>
2018-03-19 09:14:45 -0400

    Update Travis for Coverity
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/travis-tasks.sh
+++ .travis/travis-tasks.sh
@@ -8,20 +8,20 @@ pushd /builddir/
 meson --buildtype=debug travis
 
 ninja -C travis test
-cat /builddir/travis/meson-logs/testlog.txt
+if [ $? != 0 ]; then
+    cat /builddir/travis/meson-logs/testlog.txt
+fi
 
 meson --buildtype=debug coverity
 pushd coverity
-echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
 
 # The coverity scan script returns an error despite succeeding...
-curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh | \
  TRAVIS_BRANCH="${TRAVIS_BRANCH:-master}" \
  COVERITY_SCAN_PROJECT_NAME="${COVERITY_SCAN_PROJECT_NAME:-sgallagher/libmodulemd}" \
  COVERITY_SCAN_NOTIFICATION_EMAIL="${COVERITY_SCAN_NOTIFICATION_EMAIL:-sgallagh@redhat.com}" \
  COVERITY_SCAN_BUILD_COMMAND="${COVERITY_SCAN_BUILD_COMMAND:-ninja}" \
  COVERITY_SCAN_BRANCH_PATTERN=${COVERITY_SCAN_BRANCH_PATTERN:-master} \
- bash ||:
+ /usr/bin/travisci_build_coverity_scan.sh ||:
 
 popd #coverity
 
--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ MAINTAINER Stephen Gallagher <sgallagh@redhat.com>
 
 ARG TARBALL
 
-RUN dnf -y --setopt=install_weak_deps=False install git-core meson gcc ninja-build wget curl openssl popt-devel sudo pkgconf glib2-devel gobject-introspection-devel python3-gobject-base libyaml-devel gtk-doc redhat-rpm-config ruby rubygems && dnf -y clean all
+RUN dnf -y --setopt=install_weak_deps=False install git-core meson gcc ninja-build wget curl openssl popt-devel sudo pkgconf glib2-devel gobject-introspection-devel python3-gobject-base libyaml-devel gtk-doc redhat-rpm-config ruby rubygems "rubygem(json)" && dnf -y clean all
+
+RUN  echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca- && curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /usr/bin/travisci_build_coverity_scan.sh && chmod a+x /usr/bin/travisci_build_coverity_scan.sh
 
 ADD $TARBALL /builddir/
 

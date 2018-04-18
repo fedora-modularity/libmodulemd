3b86e944fdcddf7d6e00ac9a58f39b484e355346
Stephen Gallagher <sgallagh@redhat.com>
2018-04-18 14:14:04 -0400

    Add a test for default conflicts
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/test-modulemd-defaults.c
+++ modulemd/test-modulemd-defaults.c
@@ -795,6 +795,47 @@ modulemd_regressions_issue42 (DefaultsFixture *fixture,
 
 
 static void
+modulemd_regressions_issue44 (DefaultsFixture *fixture,
+                              gconstpointer user_data)
+{
+  gboolean result;
+  g_autoptr (ModulemdPrioritizer) prioritizer = NULL;
+  g_autoptr (GPtrArray) base_objects = NULL;
+  g_autoptr (GPtrArray) conflicting_objects = NULL;
+  g_autoptr (GError) error = NULL;
+  g_autofree gchar *yaml_base_path = NULL;
+  g_autofree gchar *yaml_conflicting_path = NULL;
+
+  prioritizer = modulemd_prioritizer_new ();
+
+  /* Get a simple document */
+  yaml_base_path = g_strdup_printf ("%s/test_data/defaults/issue44-1.yaml",
+                                    g_getenv ("MESON_SOURCE_ROOT"));
+  g_assert_nonnull (yaml_base_path);
+
+  base_objects = modulemd_objects_from_file (yaml_base_path, &error);
+  g_assert_nonnull (base_objects);
+
+  result = modulemd_prioritizer_add (prioritizer, base_objects, 0, &error);
+  g_assert_true (result);
+
+
+  /* Add another almost-identical document, except with a conflicting default
+   * stream set.
+   */
+  yaml_conflicting_path = g_strdup_printf ("%s/test_data/defaults/issue44-2.yaml",
+                                    g_getenv ("MESON_SOURCE_ROOT"));
+  g_assert_nonnull (yaml_conflicting_path);
+
+  conflicting_objects = modulemd_objects_from_file (yaml_conflicting_path, &error);
+  g_assert_nonnull (conflicting_objects);
+
+  result = modulemd_prioritizer_add (prioritizer, conflicting_objects, 0, &error);
+  g_assert_false (result);
+}
+
+
+static void
 modulemd_regressions_issue45 (DefaultsFixture *fixture,
                               gconstpointer user_data)
 {
@@ -891,6 +932,13 @@ main (int argc, char *argv[])
               NULL,
               NULL,
               modulemd_regressions_issue42,
+              NULL);
+
+  g_test_add ("/modulemd/defaults/modulemd_regressions_issue44",
+              DefaultsFixture,
+              NULL,
+              NULL,
+              modulemd_regressions_issue44,
               NULL);
 
   g_test_add ("/modulemd/defaults/modulemd_regressions_issue45",

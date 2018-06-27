104faf560c0c4471fd9028bbf936b78ab49b0310
Stephen Gallagher <sgallagh@redhat.com>
2018-06-27 13:50:06 -0400

    Update license headers
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/v1/tests/test-modulemd-modulestream.c
+++ modulemd/v1/tests/test-modulemd-modulestream.c
@@ -1,25 +1,14 @@
-/* test-modulemd-modulestream.c
+/*
+ * This file is part of libmodulemd
+ * Copyright (C) 2017-2018 Stephen Gallagher
  *
- * Copyright (C) 2018 Stephen Gallagher
+ * Fedora-License-Identifier: MIT
+ * SPDX-2.0-License-Identifier: MIT
+ * SPDX-3.0-License-Identifier: MIT
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * This program is free software.
+ * For more information on the license, see COPYING.
+ * For more information on free software, see <https://www.gnu.org/philosophy/free-sw.en.html>.
  */
 
 #include <glib.h>
--- modulemd/v1/tests/test-valgrind.py
+++ modulemd/v1/tests/test-valgrind.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+# This file is part of libmodulemd
+# Copyright (C) 2017-2018 Stephen Gallagher
+#
+# Fedora-License-Identifier: MIT
+# SPDX-2.0-License-Identifier: MIT
+# SPDX-3.0-License-Identifier: MIT
+#
+# This program is free software.
+# For more information on the license, see COPYING.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.
+
 import os
 import sys
 import subprocess

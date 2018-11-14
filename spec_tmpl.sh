4467a4b06ee7cdfd206f8df3e6d73d04294bc0ef
Stephen Gallagher <sgallagh@redhat.com>
2018-11-14 14:27:26 -0500

    Add v1 compatibility package to the specfile template
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- spec_tmpl.sh
+++ spec_tmpl.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 
 version=$1
-template=$2
+libmodulemd_v1_version=$2
+template=$3
 datetime=$(date +%Y%m%d%H%M%S)
 
-sed -e "s/@VERSION@/$1/" -e "s/@DATETIME@/$datetime/" $2
+sed -e "s/@VERSION@/$version/" \
+    -e "s/@V1_VERSION@/$libmodulemd_v1_version/" \
+    -e "s/@DATETIME@/$datetime/" $template

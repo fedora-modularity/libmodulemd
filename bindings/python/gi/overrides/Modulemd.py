f7f01741f0cebca2b63ccbb9d7acb7f6b428a31a
Stephen Gallagher <sgallagh@redhat.com>
2018-11-26 14:15:47 -0500

    Add ModuleStreamV{12} objects
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

---
+++ bindings/python/gi/overrides/Modulemd.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+
+# This file is part of libmodulemd
+# Copyright (C) 2016 Red Hat, Inc.
+# Copyright (C) 2017-2018 Stephen Gallagher
+#
+# Fedora-License-Identifier: MIT
+# SPDX-2.0-License-Identifier: MIT
+# SPDX-3.0-License-Identifier: MIT
+#
+# This program is free software.
+# For more information on the license, see COPYING.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.#!/usr/bin/python3
+
+# This file is part of libmodulemd
+# Copyright (C) 2017-2018 Stephen Gallagher
+#
+# Fedora-License-Identifier: MIT
+# SPDX-2.0-License-Identifier: MIT
+# SPDX-3.0-License-Identifier: MIT
+#
+# This program is free software.
+# For more information on the license, see COPYING.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.
+
+
+import gi
+
+from ..module import get_introspection_module
+from ..overrides import override
+
+Modulemd = get_introspection_module('Modulemd')
+
+from six import text_type
+from gi.repository import GLib
+
+__all__ = []
+
+class ModulemdUtil():
+    def variant_str(s):
+        """ Converts a string to a GLib.Variant
+        """
+        if not isinstance(s, str):
+            raise TypeError('Only strings are supported for scalars')
+
+        return GLib.Variant('s', s)
+
+    def variant_bool(b):
+        """ Converts a boolean to a GLib.Varant
+        """
+        if not isinstance(b, bool):
+            raise TypeError('Only booleans are supported')
+
+        return GLib.Variant('b', b)
+
+    def variant_list(l):
+        """ Converts a list to a GLib.Variant
+        """
+        l_variant = list()
+        for item in l:
+            if item is None:
+                item = ''
+
+            l_variant.append(ModulemdUtil.python_to_variant(item))
+        return GLib.Variant('av', l_variant)
+
+    def variant_dict(d):
+        """ Converts a dictionary to a dictionary of GLib.Variant
+        """
+        if not isinstance(d, dict):
+            raise TypeError('Only dictionaries are supported for mappings')
+
+        d_variant = dict_values(d)
+        return GLib.Variant('a{sv}', d_variant)
+
+    def dict_values(d):
+        """ Converts each dictionary value to a GLib.Variant
+        """
+        if not isinstance(d, dict):
+            raise TypeError('Only dictionaries are supported for mappings')
+
+        d_variant = dict()
+        for k, v in d.items():
+            if v is None:
+                v = ''
+
+            d_variant[k] = ModulemdUtil.python_to_variant(v)
+        return d_variant
+
+    def python_to_variant(obj):
+        if isinstance(obj, str):
+            return ModulemdUtil.variant_str(obj)
+
+        elif isinstance(obj, text_type):
+            return ModulemdUtil.variant_str(obj.encode('utf-8'))
+
+        elif isinstance(obj, bool):
+            return ModulemdUtil.variant_bool(obj)
+
+        elif isinstance(obj, list):
+            return ModulemdUtil.variant_list(obj)
+
+        elif isinstance(obj, dict):
+            return ModulemdUtil.variant_dict(obj)
+
+        else:
+            raise TypeError('Cannot convert unknown type')
+
+
+if "ModuleStreamV2" in dir(Modulemd):
+    class ModuleStreamV2(Modulemd.ModuleStreamV2):
+
+        def __new__(cls, module_name=None, module_stream=None):
+            return Modulemd.ModuleStreamV2.new(module_name, module_stream)
+
+        def set_xmd(self, xmd):
+            super().set_xmd(ModulemdUtil.python_to_variant(xmd))
+
+        def get_xmd(self):
+            variant_xmd = super().get_xmd()
+            return variant_xmd.unpack()
+
+
+    ModuleStreamV2 = override(ModuleStreamV2)
+    __all__.append(ModuleStreamV2)
+
+
+if "ModuleStreamV1" in dir(Modulemd):
+    class ModuleStreamV1(Modulemd.ModuleStreamV1):
+
+        def __new__(cls, module_name=None, module_stream=None):
+            return Modulemd.ModuleStreamV1.new(module_name, module_stream)
+
+        def set_xmd(self, xmd):
+            super().set_xmd(ModulemdUtil.python_to_variant(xmd))
+
+        def get_xmd(self):
+            variant_xmd = super().get_xmd()
+            return variant_xmd.unpack()
+
+
+    ModuleStreamV1 = override(ModuleStreamV1)
+    __all__.append(ModuleStreamV1)
+
--- modulemd/v2/tests/ModulemdTests/modulestream.py
+++ modulemd/v2/tests/ModulemdTests/modulestream.py
@@ -1,8 +1,11 @@
+import os
 import sys
 try:
     import unittest
     import gi
+
     gi.require_version('Modulemd', '2.0')
+    from gi.repository import GLib
     from gi.repository import Modulemd
 except ImportError:
     # Return error 77 to skip this test on platforms without the necessary
@@ -174,6 +177,327 @@ class TestModuleStream(TestBase):
             # Add a context
             stream.props.context = 'deadbeef'
             assert stream.get_nsvc_as_string() == 'modulename:streamname:42:deadbeef'
+
+    def test_arch(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.arch is None
+            assert stream.get_arch() is None
+
+            # Test property setting
+            stream.props.arch = 'x86_64'
+            assert stream.props.arch == 'x86_64'
+            assert stream.get_arch() == 'x86_64'
+
+            # Test set_arch()
+            stream.set_arch('ppc64le')
+            assert stream.props.arch == 'ppc64le'
+            assert stream.get_arch() == 'ppc64le'
+
+            # Test setting it to None
+            stream.props.arch = None
+            assert stream.props.arch is None
+            assert stream.get_arch() is None
+
+    def test_buildopts(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.buildopts is None
+            assert stream.get_buildopts() is None
+
+            buildopts = Modulemd.Buildopts()
+            buildopts.props.rpm_macros = '%demomacro 1'
+            stream.set_buildopts(buildopts)
+
+            assert stream.props.buildopts is not None
+            assert stream.props.buildopts is not None
+            assert stream.props.buildopts.props.rpm_macros == '%demomacro 1'
+
+    def test_community(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.community is None
+            assert stream.get_community() is None
+
+            # Test property setting
+            stream.props.community = 'http://example.com'
+            assert stream.props.community == 'http://example.com'
+            assert stream.get_community() == 'http://example.com'
+
+            # Test set_community()
+            stream.set_community('http://redhat.com')
+            assert stream.props.community == 'http://redhat.com'
+            assert stream.get_community() == 'http://redhat.com'
+
+            # Test setting it to None
+            stream.props.community = None
+            assert stream.props.community is None
+            assert stream.get_community() is None
+
+    def test_description(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.description is None
+            assert stream.get_description() is None
+
+            # Test property setting
+            stream.props.description = 'A description'
+            assert stream.props.description == 'A description'
+            assert stream.get_description() == 'A description'
+
+            # Test set_description()
+            stream.set_description('A different description')
+            assert stream.props.description == 'A different description'
+            assert stream.get_description() == 'A different description'
+
+            # Test setting it to None
+            stream.props.description = None
+            assert stream.props.description is None
+            assert stream.get_description() is None
+
+    def test_documentation(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.documentation is None
+            assert stream.get_documentation() is None
+
+            # Test property setting
+            stream.props.documentation = 'http://example.com'
+            assert stream.props.documentation == 'http://example.com'
+            assert stream.get_documentation() == 'http://example.com'
+
+            # Test set_documentation()
+            stream.set_documentation('http://redhat.com')
+            assert stream.props.documentation == 'http://redhat.com'
+            assert stream.get_documentation() == 'http://redhat.com'
+
+            # Test setting it to None
+            stream.props.documentation = None
+            assert stream.props.documentation is None
+            assert stream.get_documentation() is None
+
+    def test_summary(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.summary is None
+            assert stream.get_summary() is None
+
+            # Test property setting
+            stream.props.summary = 'A summary'
+            assert stream.props.summary == 'A summary'
+            assert stream.get_summary() == 'A summary'
+
+            # Test set_summary()
+            stream.set_summary('A different summary')
+            assert stream.props.summary == 'A different summary'
+            assert stream.get_summary() == 'A different summary'
+
+            # Test setting it to None
+            stream.props.summary = None
+            assert stream.props.summary is None
+            assert stream.get_summary() is None
+
+    def test_tracker(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Check the defaults
+            assert stream.props.tracker is None
+            assert stream.get_tracker() is None
+
+            # Test property setting
+            stream.props.tracker = 'http://example.com'
+            assert stream.props.tracker == 'http://example.com'
+            assert stream.get_tracker() == 'http://example.com'
+
+            # Test set_tracker()
+            stream.set_tracker('http://redhat.com')
+            assert stream.props.tracker == 'http://redhat.com'
+            assert stream.get_tracker() == 'http://redhat.com'
+
+            # Test setting it to None
+            stream.props.tracker = None
+            assert stream.props.tracker is None
+            assert stream.get_tracker() is None
+
+    def test_components(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            # Add an RPM component to a stream
+            rpm_comp = Modulemd.ComponentRpm(name='rpmcomponent')
+            stream.add_component(rpm_comp)
+            assert 'rpmcomponent' in stream.get_rpm_component_names_as_strv()
+            retrieved_comp = stream.get_rpm_component('rpmcomponent')
+            assert retrieved_comp
+            assert retrieved_comp.props.name == 'rpmcomponent'
+
+            # Add a Module component to a stream
+            mod_comp = Modulemd.ComponentModule(name='modulecomponent')
+            stream.add_component(mod_comp)
+            assert 'modulecomponent' in stream.get_module_component_names_as_strv()
+            retrieved_comp = stream.get_module_component('modulecomponent')
+            assert retrieved_comp
+            assert retrieved_comp.props.name == 'modulecomponent'
+
+            # Remove an RPM component from a stream
+            stream.remove_rpm_component('rpmcomponent')
+
+            # Remove a Module component from a stream
+            stream.remove_module_component('modulecomponent')
+
+    def test_licenses(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            stream.add_content_license('GPLv2+')
+            assert 'GPLv2+' in stream.get_content_licenses_as_strv()
+
+            stream.add_module_license('MIT')
+            assert 'MIT' in stream.get_module_licenses_as_strv()
+
+            stream.remove_content_license('GPLv2+')
+            stream.remove_module_license('MIT')
+
+    def test_profiles(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version, 'sssd')
+
+            profile = Modulemd.Profile(name='client')
+            profile.add_rpm('sssd-client')
+
+            stream.add_profile(profile)
+            assert len(stream.get_profile_names_as_strv()) == 1
+            assert 'client' in stream.get_profile_names_as_strv()
+            assert 'sssd-client' in stream.get_profile(
+                'client').get_rpms_as_strv()
+
+            stream.clear_profiles()
+            assert len(stream.get_profile_names_as_strv()) == 0
+
+    def test_rpm_api(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version, 'sssd')
+
+            stream.add_rpm_api('sssd-common')
+            assert 'sssd-common' in stream.get_rpm_api_as_strv()
+
+            stream.remove_rpm_api('sssd-common')
+            assert len(stream.get_rpm_api_as_strv()) == 0
+
+    def test_rpm_artifacts(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            stream.add_rpm_artifact('bar-0:1.23-1.module_deadbeef.x86_64')
+            assert 'bar-0:1.23-1.module_deadbeef.x86_64' in stream.get_rpm_artifacts_as_strv()
+
+            stream.remove_rpm_artifact('bar-0:1.23-1.module_deadbeef.x86_64')
+            assert len(stream.get_rpm_artifacts_as_strv()) == 0
+
+    def test_rpm_filters(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+
+            stream.add_rpm_filter('bar')
+            assert 'bar' in stream.get_rpm_filters_as_strv()
+
+            stream.remove_rpm_filter('bar')
+            assert len(stream.get_rpm_filters_as_strv()) == 0
+
+    def test_servicelevels(self):
+        for version in modulestream_versions:
+            stream = Modulemd.ModuleStream.new(version)
+            sl = Modulemd.ServiceLevel.new('rawhide')
+            sl.set_eol_ymd(1980, 3, 2)
+
+            stream.add_servicelevel(sl)
+
+            assert 'rawhide' in stream.get_servicelevel_names_as_strv()
+
+            retrieved_sl = stream.get_servicelevel('rawhide')
+            assert retrieved_sl.props.name == 'rawhide'
+            assert retrieved_sl.get_eol_as_string() == '1980-03-02'
+
+    def test_v1_eol(self):
+        stream = Modulemd.ModuleStreamV1.new()
+        eol = GLib.Date.new_dmy(3, 2, 1998)
+
+        stream.set_eol(eol)
+        retrieved_eol = stream.get_eol()
+
+        assert retrieved_eol.get_day() == 3
+        assert retrieved_eol.get_month() == 2
+        assert retrieved_eol.get_year() == 1998
+
+        sl = stream.get_servicelevel('rawhide')
+        assert sl.get_eol_as_string() == '1998-02-03'
+
+    def test_v1_dependencies(self):
+        stream = Modulemd.ModuleStreamV1.new()
+        stream.add_buildtime_requirement('testmodule', 'stable')
+
+        assert len(stream.get_buildtime_modules_as_strv()) == 1
+        assert 'testmodule' in stream.get_buildtime_modules_as_strv()
+
+        assert stream.get_buildtime_requirement_stream('testmodule') == \
+            'stable'
+
+        stream.add_runtime_requirement('testmodule', 'latest')
+        assert len(stream.get_runtime_modules_as_strv()) == 1
+        assert 'testmodule' in stream.get_runtime_modules_as_strv()
+        assert stream.get_runtime_requirement_stream('testmodule') == 'latest'
+
+    def test_v2_dependencies(self):
+        stream = Modulemd.ModuleStreamV2.new()
+        deps = Modulemd.Dependencies()
+
+        deps.add_buildtime_stream('foo', 'stable')
+        deps.set_empty_runtime_dependencies_for_module('bar')
+        stream.add_dependencies(deps)
+
+        assert len(stream.get_dependencies()) == 1
+        assert len(stream.get_dependencies()) == 1
+
+        assert 'foo' in stream.get_dependencies(
+        )[0].get_buildtime_modules_as_strv()
+
+        assert 'stable' in stream.get_dependencies(
+        )[0].get_buildtime_streams_as_strv('foo')
+
+        assert 'bar' in stream.get_dependencies(
+        )[0].get_runtime_modules_as_strv()
+
+    def test_xmd(self):
+        if os.getenv('MMD_TEST_INSTALLED_LIB'):
+            # The XMD python tests can only be run against the installed lib
+            # because the overrides that translate between python and GVariant
+            # must be installed in /usr/lib/python*/site-packages/gi/overrides
+            # or they are not included when importing Modulemd
+            stream = Modulemd.ModuleStreamV2.new()
+
+            xmd = {'outer_key': ['scalar', {'inner_key': 'another_scalar'}]}
+
+            stream.set_xmd(xmd)
+
+            xmd_copy = stream.get_xmd()
+            assert xmd_copy
+            assert 'outer_key' in xmd
+            assert 'scalar' in xmd['outer_key']
+            assert 'inner_key' in xmd['outer_key'][1]
+            assert xmd['outer_key'][1]['inner_key'] == 'another_scalar'
 
     def test_yaml(self):
         for version in modulestream_versions:

4c56271d1ea2cf0b9daa1a3cb613200009032c5b
Stephen Gallagher <sgallagh@redhat.com>
2019-02-13 15:53:21 -0500

    Fix Coverity scan detection
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/coverity_prep.sh
+++ .travis/coverity_prep.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
+set -x
+
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
 
 curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /usr/bin/travisci_build_coverity_scan.sh
 
-if [ "$(file /usr/bin/travisci_build_coverity_scan.sh)" == "text/x-shellscript" ]; then
+if [ "$(file -b --mime-type /usr/bin/travisci_build_coverity_scan.sh)" == "text/x-shellscript" ]; then
     chmod a+x /usr/bin/travisci_build_coverity_scan.sh
 else
     echo "Warning: Coverity not detected!"

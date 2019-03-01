c1840e4c08193543d9066df4e784b008a1f0e5fc
indigoomega021 <indigoomega021@localhost.localdomain>
2019-03-01 11:08:21 -0500

    completed Modulemd.ModuleStream equals function

---
+++ api2/make_srpm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+# optionally call this function with --nocheck to skip the test suite while
+# building RPMs.
+
+set -e
+
+echo "Creating tarball. This may take a while."
+
+MMD_SKIP_VALGRIND=True ninja dist > /dev/null
+
+ln -sf ../meson-dist/ ./rpmbuild/SOURCES
+
+rpmbuild -bs $1 libmodulemd.spec --define "_topdir $(pwd)/rpmbuild"

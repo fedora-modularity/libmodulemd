740dcf1bebe1af4c7b043f1217354f27d0a86664
Stephen Gallagher <sgallagh@redhat.com>
2019-04-05 08:10:32 -0400

    Update CentOS tests for Python 3.6
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/centos/travis-tasks.sh
+++ .travis/centos/travis-tasks.sh
@@ -14,7 +14,7 @@ release=${arr[1]:-7}
 # CentOS 7 doesn't have autopep8, so we'll drop the requirement for it
 # This implementation will still allow it to occur if autopep8 still shows
 # up later.
-COMMON_MESON_ARGS="-Dtest_dirty_git=false -Ddeveloper_build=false -Dpython_name=python3.4"
+COMMON_MESON_ARGS="-Dtest_dirty_git=false -Ddeveloper_build=false -Dpython_name=python3.6"
 
 pushd /builddir/
 

e0d6feb89c978dca94a2de36b6b66f5ebb0b361f
Stephen Gallagher <sgallagh@redhat.com>
2019-04-29 08:03:12 -0400

    Work around Coverity certificate problem
    As noted in Travis CI issue
    https://travis-ci.community/t/certificate-issue-during-coverity-build/3153/2
    the certificate chain for scan.coverity.com is missing an intermediate
    certificate, which makes it fail to validate for curl. This patch
    downloads the missing certificate and adds it to the system store to
    work around the issue.
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/coverity_prep.sh
+++ .travis/coverity_prep.sh
@@ -3,6 +3,9 @@
 set -x
 
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
+curl -s -L https://entrust.com/root-certificates/entrust_l1k.cer | tee -a /etc/pki/ca-trust/source/anchors/entrust_l1k.cer
+sudo update-ca-trust
+
 
 curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /usr/bin/travisci_build_coverity_scan.sh
 

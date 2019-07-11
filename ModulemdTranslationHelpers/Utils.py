4bb0fbc7f8d8511466798e8cc3e01f4cb4f6f269
orionstar25 <chunnushrivastava@gmail.com>
2019-07-11 20:01:44 +0530

    Add translations in Modulemd.Index from Babel Catalog

--- ModulemdTranslationHelpers/Utils.py
+++ ModulemdTranslationHelpers/Utils.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
-
+import sys
 from babel.messages import Catalog, pofile
-from babel._compat import StringIO, BytesIO
 from datetime import datetime
 from collections import defaultdict
+
+try:
+    import gi
+    gi.require_version('Modulemd', '2.0')
+    from gi.repository import Modulemd
+except ImportError:
+    sys.exit(77)
 
 
 def get_translation_catalog_from_index(index, project_name):
@@ -31,13 +36,13 @@ def get_translation_catalog_from_index(index, project_name):
     for stream in final_streams:
         # Process description
         description = stream.get_description("C")
-        location = ("{}:{};description").format(
+        location = ("{};{};description").format(
             stream.props.module_name, stream.props.stream_name)
         translation_dict[description].append(location)
 
         # Process summary
         summary = stream.get_summary("C")
-        location = ("{}:{};summary").format(
+        location = ("{};{};summary").format(
             stream.props.module_name, stream.props.stream_name)
         translation_dict[summary].append(location)
 
@@ -48,7 +53,7 @@ def get_translation_catalog_from_index(index, project_name):
                 profile = stream.get_profile(pro_name)
 
                 profile_desc = profile.get_description("C")
-                location = ("{}:{};profile;{}").format(
+                location = ("{};{};profile;{}").format(
                     stream.props.module_name, stream.props.stream_name, pro_name)
                 translation_dict[profile_desc].append(location)
 
@@ -58,3 +63,77 @@ def get_translation_catalog_from_index(index, project_name):
         catalog.add(translatable_string, locations=locations)
 
     return catalog
+
+
+def get_modulemd_translations_from_catalog(catalogs, index):
+    # Dictionary `translations` contains information from catalog like:
+    # Key: (module_name, stream_name)
+    # Value: Translation object containing TranslationEntry of various locales
+    translations = dict()
+
+    now = datetime.utcnow()
+    modified = int(now.strftime("%Y%m%d%H%M%S"))
+
+    # Handling one language translations at a time
+    for catalog in catalogs:
+        # Dictionary `data` contains information from catalog like:
+        # Key: (module_name, stream_name)
+        # Value: TranslationEntry object of a locale
+        data = dict()
+
+        for msg in catalog:
+            for location, _ in msg.locations:
+                (module_name, stream_name, string_type,
+                 profile_name) = split_location(location)
+
+                try:
+                    entry = data[(module_name, stream_name)]
+                except KeyError:
+                    entry = Modulemd.TranslationEntry.new(
+                        str(catalog.locale))
+
+                if string_type == "summary":
+                    entry.set_summary(msg.string)
+                elif string_type == "description":
+                    entry.set_description(msg.string)
+                else:
+                    entry.set_profile_description(profile_name, msg.string)
+
+                data[(module_name, stream_name)] = entry
+
+        for (module_name, stream_name), entry in data.items():
+            try:
+                mmd_translation = translations[(module_name, stream_name)]
+            except KeyError:
+                mmd_translation = Modulemd.Translation.new(
+                    1, module_name, stream_name, modified)
+
+            mmd_translation.set_translation_entry(entry)
+            translations[(module_name, stream_name)] = mmd_translation
+
+    for (module_name, stream_name), mmd_translation in translations.items():
+        try:
+            ret = index.add_translation(mmd_translation)
+        except GLib.Error:
+            print(
+                "The translation for this %s:%s could not be added",
+                module_name,
+                stream_name)
+
+    return None
+
+
+def split_location(location):
+    # Split maximum three times
+    props = location.split(";", 3)
+
+    module_name = props[0]
+    stream_name = props[1]
+    string_type = props[2]
+
+    if string_type == "profile":
+        profile_name = props[3]
+    else:
+        profile_name = None
+
+    return module_name, stream_name, string_type, profile_name

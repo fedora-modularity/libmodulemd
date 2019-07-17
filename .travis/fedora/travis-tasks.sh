2d461725f781c6fdcf32893d8dcfa40bcef8dda5
Stephen Gallagher <sgallagh@redhat.com>
2019-07-17 16:20:30 -0400

    Create v1-only branch
    This branch will be maintenance-only for libmodulemd 1.x.
    
    This patch drops all v2 code from the tree and reworks the
    build system and CI tests for this branch.
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/fedora/travis-tasks.sh
+++ .travis/fedora/travis-tasks.sh
@@ -16,10 +16,8 @@ COMMON_MESON_ARGS="-Dtest_dirty_git=${DIRTY_REPO_CHECK:-true}"
 
 pushd /builddir/
 
-# Build the v1 and v2 code under GCC and run standard tests
+# Build the code under GCC and run standard tests
 meson --buildtype=debug \
-      -Dbuild_api_v1=true \
-      -Dbuild_api_v2=true \
       $COMMON_MESON_ARGS \
       travis
 
@@ -32,34 +30,11 @@ if [ $ret != 0 ]; then
 fi
 set -e
 
-# Test the v2 code with clang-analyzer
-# This requires meson 0.49.0 or later
-set +e
-rpmdev-vercmp `meson --version` 0.49.0
-if [ $? -eq 12 ]; then
-    # Meson was older than 0.49.0, skip this step
-    echo "Meson is too old to run scan-build"
-    set -e
-else
-    set -e
-    meson --buildtype=debug \
-          -Dbuild_api_v1=false \
-          -Dbuild_api_v2=true \
-          -Dskip_introspection=true \
-          $COMMON_MESON_ARGS \
-          travis_scanbuild
-
-    pushd travis_scanbuild
-    /builddir/.travis/scanbuild.sh
-    popd #travis_scanbuild
-fi
-
 
 # Always install and run the installed RPM tests last so we don't pollute the
 # testing environment above.
 
 meson --buildtype=debug \
-      -Dbuild_api_v1=true \
       $COMMON_MESON_ARGS \
       build_rpm
 
@@ -86,47 +61,15 @@ dnf -y install --nogpgcheck \
 popd #build_rpm
 
 meson --buildtype=debug \
-      -Dbuild_api_v1=true \
-      -Dbuild_api_v2=false \
       -Dtest_installed_lib=true \
       $COMMON_MESON_ARGS \
-      installed_lib_tests_v1
+      installed_lib_tests
 
-pushd installed_lib_tests_v1
+pushd installed_lib_tests
 
 # Run the tests against the installed RPMs
 ninja test
 
-popd #installed_lib_tests_v1
-
-
-pushd build_rpm
-
-dnf -y install --nogpgcheck \
-               --allowerasing \
-               --repofrompath libmodulemd-travis,rpmbuild/RPMS \
-               python3-libmodulemd \
-               "libmodulemd-devel > 2"
-
-# Also install the python2-libmodulemd if it was built for this release
-# the ||: at the end instructs bash to consider this a pass either way.
-dnf -y install --nogpgcheck \
-               --allowerasing \
-               --repofrompath libmodulemd-travis,rpmbuild/RPMS \
-               python2-libmodulemd ||:
-popd
-
-meson --buildtype=debug \
-      -Dbuild_api_v1=false \
-      -Dbuild_api_v2=true \
-      -Dtest_installed_lib=true \
-      $COMMON_MESON_ARGS \
-      installed_lib_tests_v2
-
-pushd installed_lib_tests_v2
-# Run the tests against the installed RPMs
-ninja test
-
-popd #installed_lib_tests_v2
+popd #installed_lib_tests
 
 popd #builddir

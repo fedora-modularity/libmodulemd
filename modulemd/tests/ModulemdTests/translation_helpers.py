822e50006333ffdc717a152dbe17f94acbc98b92
orionstar25 <chunnushrivastava@gmail.com>
2019-08-03 00:45:51 +0530

    Retrieve metadata from koji and feed into translation helpers

--- modulemd/tests/ModulemdTests/translation_helpers.py
+++ modulemd/tests/ModulemdTests/translation_helpers.py
@@ -14,12 +14,13 @@
 
 import os
 import sys
+import unittest
 from ModulemdTranslationHelpers import Utils
 from babel.messages import pofile
 from six import text_type
+from unittest import mock
 
 try:
-    import unittest
     import gi
     gi.require_version('Modulemd', '2.0')
     from gi.repository import Modulemd
@@ -29,6 +30,61 @@ except ImportError:
     sys.exit(77)
 
 from base import TestBase
+
+
+class KojiSessionMock:
+
+    def __init__(self):
+        with open("%s/f29_build_1.yaml" % (os.getenv('MESON_SOURCE_ROOT')), 'r') as file:
+            self.build_1_yaml = file.read()
+
+        with open("%s/f29_build_2.yaml" % (os.getenv('MESON_SOURCE_ROOT')), 'r') as file:
+            self.build_2_yaml = file.read()
+
+    def listTagged(self, tag):
+        tagged_builds = [{
+            'id': 1,
+            'name': 'foo',
+            'version': 'master',
+            'release': '10.1'
+        },
+            {
+            'id': 2,
+            'name': 'bar',
+            'version': 'master',
+            'release': '2.1'
+        }]
+        return tagged_builds
+
+    def getBuild(self, build_id):
+        build = [{
+            'id': 1,
+            'package_name': 'marvel',
+            'nvr': 'black',
+            'extra': {
+                'typeinfo': {
+                    'module': {
+                        'modulemd_str': self.build_1_yaml
+                    }
+                }
+            }
+        },
+            {
+            'id': 2,
+            'package_name': 'marvel',
+            'nvr': 'black',
+            'extra': {
+                'typeinfo': {
+                    'module': {
+                        'modulemd_str': self.build_2_yaml
+                    }
+                }
+            }
+        }]
+
+        if build_id == 1:
+            return build[0]
+        return build[1]
 
 
 class TestTranslationHelpers(TestBase):
@@ -141,6 +197,49 @@ class TestTranslationHelpers(TestBase):
                             self.assertEqual(
                                 profile.get_description(
                                     str(catalog.locale)), msg_string)
+
+    @mock.patch('koji.ClientSession')
+    def test_index_from_tags(self, mock_session):
+        koji_session_mock = KojiSessionMock()
+        tags = ['f29']
+
+        # Create a Modulemd.ModuleIndex object containing the YAML stream
+        idx = Modulemd.ModuleIndex.new()
+        self.assertIsNotNone(idx)
+        ret, failures = idx.update_from_file(
+            "%s/builds.yaml" %
+            (os.getenv('MESON_SOURCE_ROOT')), True)
+        self.assertTrue(ret)
+        self.assertTrue(len(failures) == 0)
+
+        # Test when koji returns no modules
+        mock_session.listTagged.return_value = []
+        mock_session.getBuild.return_value = None
+
+        index = Utils.get_index_from_tags(mock_session, tags)
+        self.assertIsNotNone(index)
+        mock_session.listTagged.assert_called_once_with('f29')
+        self.assertListEqual(index.get_module_names(), [])
+        mock_session.reset_mock()
+
+        # Test when koji returns proper modules
+        mock_session.listTagged.side_effect = koji_session_mock.listTagged
+        mock_session.getBuild.side_effect = koji_session_mock.getBuild
+
+        index = Utils.get_index_from_tags(mock_session, tags)
+        self.assertIsNotNone(index)
+        mock_session.listTagged.assert_called_once_with('f29')
+        mock_session.getBuild.assert_called_with(2)
+        self.assertEqual(idx.dump_to_string(), index.dump_to_string())
+        self.assertListEqual(index.get_module_names(), ['ant', 'avocado'])
+        module = index.get_module('ant')
+        self.assertIsNotNone(module)
+        stream = module.get_stream_by_NSVCA(
+            "1.10", 20180629154141, "819b5873")
+        self.assertIsNotNone(stream)
+        self.assertEqual(stream.get_nsvc(),
+                         "ant:1.10:20180629154141:819b5873")
+        self.assertEqual(stream.get_summary(None), "Java build tool")
 
 
 if __name__ == '__main__':

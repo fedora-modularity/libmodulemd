9926438ee149c5e3aa00418ce2f4c3811dcf4dda
Merlin Mathesius <mmathesi@redhat.com>
2019-08-27 09:38:58 -0500

    Refactor stream copy/upgrade helper macros. Another final batch of documentation updates for private functions.
    Signed-off-by: Merlin Mathesius <mmathesi@redhat.com>

--- modulemd/include/private/modulemd-module-stream-private.h
+++ modulemd/include/private/modulemd-module-stream-private.h
@@ -113,47 +113,161 @@ modulemd_module_stream_validate_components (GHashTable *components,
 
 
 /* Some macros used for copy operations */
-#define STREAM_COPY_IF_SET(version, dest, src, property)                      \
+/**
+ * STREAM_UPGRADE_IF_SET_FULL:
+ * @oldversion: The stream version of @src. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @newversion: The stream version of @dest. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the property to copy. Must be the literal property
+ * name, in lower case, without quotes.
+ * @locale...: (in): An optional locale that can be provided when @property has
+ * possible translations.
+ *
+ * DIRECT USE OF THIS MACRO SHOULD BE AVOIDED. This is the internal
+ * implementation for %STREAM_COPY_IF_SET, %STREAM_UPGRADE_IF_SET,
+ * %STREAM_COPY_IF_SET_WITH_LOCALE, and %STREAM_UPGRADE_IF_SET_WITH_LOCALE
+ * which should be used instead.
+ *
+ * This is a helper macro to simplify the coding when copying/upgrading
+ * properties between #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2
+ * objects.
+ *
+ * Does nothing if the @src @property is NULL.
+ *
+ * Since: 2.0
+ */
+#define STREAM_UPGRADE_IF_SET_FULL(                                           \
+  oldversion, newversion, dest, src, property, locale...)                     \
   do                                                                          \
     {                                                                         \
-      if (modulemd_module_stream_##version##_get_##property (src) != NULL)    \
-        modulemd_module_stream_##version##_set_##property (                   \
-          dest, modulemd_module_stream_##version##_get_##property (src));     \
-    }                                                                         \
-  while (0)
-
-#define STREAM_UPGRADE_IF_SET(oldversion, newversion, dest, src, property)    \
-  do                                                                          \
-    {                                                                         \
-      if (modulemd_module_stream_##oldversion##_get_##property (src) != NULL) \
+      if (modulemd_module_stream_##oldversion##_get_##property (              \
+            src, ##locale) != NULL)                                           \
         modulemd_module_stream_##newversion##_set_##property (                \
-          dest, modulemd_module_stream_##oldversion##_get_##property (src));  \
-    }                                                                         \
-  while (0)
-
-#define STREAM_COPY_IF_SET_WITH_LOCALE(version, dest, src, property)          \
-  do                                                                          \
-    {                                                                         \
-      if (modulemd_module_stream_##version##_get_##property (src, "C") !=     \
-          NULL)                                                               \
-        modulemd_module_stream_##version##_set_##property (                   \
           dest,                                                               \
-          modulemd_module_stream_##version##_get_##property (src, "C"));      \
+          modulemd_module_stream_##oldversion##_get_##property (src,          \
+                                                                ##locale));   \
     }                                                                         \
   while (0)
 
+/**
+ * STREAM_COPY_IF_SET:
+ * @version: The stream version being copied. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the property to copy. Must be the literal property
+ * name, in lower case, without quotes.
+ *
+ * This is a convenience macro to simplify the coding when copying properties
+ * between #ModulemdModuleStream objects when both @src and @dest are the same
+ * version.
+ *
+ * Does nothing if the @src @property is NULL.
+ *
+ * Since: 2.0
+ */
+#define STREAM_COPY_IF_SET(version, dest, src, property)                      \
+  STREAM_UPGRADE_IF_SET_FULL (version, version, dest, src, property)
+
+/**
+ * STREAM_UPGRADE_IF_SET:
+ * @oldversion: The stream version of @src. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @newversion: The stream version of @dest. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the property to copy. Must be the literal property
+ * name, in lower case, without quotes.
+ *
+ * This is a convenience macro to simplify the coding when copying properties
+ * between #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2 objects when
+ * @src and @dest are different versions.
+ *
+ * Does nothing if the @src @property is NULL.
+ *
+ * Since: 2.0
+ */
+#define STREAM_UPGRADE_IF_SET(oldversion, newversion, dest, src, property)    \
+  STREAM_UPGRADE_IF_SET_FULL (oldversion, newversion, dest, src, property)
+
+/**
+ * STREAM_COPY_IF_SET_WITH_LOCALE:
+ * @version: The stream version being copied. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the property to copy. Must be the literal property
+ * name, in lower case, without quotes.
+ *
+ * This is a convenience macro to simplify the coding when copying properties
+ * between #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2 objects when
+ * both @src and @dest are the same version and @property has possible
+ * translations. Only the untranslated (`"C"` locale) version of @property will
+ * be copied.
+ *
+ * Does nothing if the @src @property is NULL.
+ *
+ * Since: 2.0
+ */
+#define STREAM_COPY_IF_SET_WITH_LOCALE(version, dest, src, property)          \
+  STREAM_UPGRADE_IF_SET_FULL (version, version, dest, src, property, "C")
+
+/**
+ * STREAM_UPGRADE_IF_SET_WITH_LOCALE:
+ * @oldversion: The stream version of @src. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @newversion: The stream version of @dest. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the property to copy. Must be the literal property
+ * name, in lower case, without quotes.
+ *
+ * This is a convenience macro to simply the coding when copying properties
+ * between #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2 objects when
+ * @src and @dest are different versions and @property has possible
+ * translations. Only the untranslated (`"C"` locale) version of @property will
+ * be copied.
+ *
+ * Does nothing if the @src @property is NULL.
+ *
+ * Since: 2.0
+ */
 #define STREAM_UPGRADE_IF_SET_WITH_LOCALE(                                    \
   oldversion, newversion, dest, src, property)                                \
-  do                                                                          \
-    {                                                                         \
-      if (modulemd_module_stream_##oldversion##_get_##property (src, "C") !=  \
-          NULL)                                                               \
-        modulemd_module_stream_##newversion##_set_##property (                \
-          dest,                                                               \
-          modulemd_module_stream_##oldversion##_get_##property (src, "C"));   \
-    }                                                                         \
-  while (0)
+  STREAM_UPGRADE_IF_SET_FULL (oldversion, newversion, dest, src, property, "C")
 
+/**
+ * STREAM_REPLACE_HASHTABLE:
+ * @version: The stream version being replaced. Must be literal "v1" or "v2"
+ * without the quotes.
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination at which @property is being replaced.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is being replaced.
+ * @property: The name of the #GHashTable property to replace. Must be the
+ * literal property name, in lower case, without quotes.
+ *
+ * This is a convenience macro to simply the coding when replacing #GHashTable
+ * properties of #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2 objects
+ * when both @src and @dest are the same version.
+ *
+ * Since: 2.0
+ */
 #define STREAM_REPLACE_HASHTABLE(version, dest, src, property)                \
   do                                                                          \
     {                                                                         \
@@ -162,6 +276,23 @@ modulemd_module_stream_validate_components (GHashTable *components,
     }                                                                         \
   while (0)
 
+/**
+ * COPY_HASHTABLE_BY_VALUE_ADDER:
+ * @dest: (out): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object
+ * that is the destination to which @property is to be copied.
+ * @src: (in): A #ModulemdModuleStreamV1 or #ModulemdModuleStreamV2 object that
+ * is the source from which @property is to be copied.
+ * @property: The name of the #GHashTable property to copy. Must be the literal
+ * property name, in lower case, without quotes.
+ * @adder: (in): A pointer to a method of @dest that supports add-on property
+ * values.
+ *
+ * This is a convenience macro to simply the coding when copying #GHashTable
+ * properties between #ModulemdModuleStreamV1 and #ModulemdModuleStreamV2
+ * objects when the property is set by using add-on values.
+ *
+ * Since: 2.0
+ */
 #define COPY_HASHTABLE_BY_VALUE_ADDER(dest, src, property, adder)             \
   do                                                                          \
     {                                                                         \

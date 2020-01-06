784f2754fe5e45ee6b9b1aede155048f51a16c39
Merlin Mathesius <mmathesi@redhat.com>
2020-01-06 08:48:36 -0500

    Detect GDate autoptr when available.
    The GDate autoptr cleanup was finally officially added in
    GLib 2.63.3--which is now available in Fedora Rawhide--so stop
    defining it ourselves.
    
    https://gitlab.gnome.org/GNOME/glib/merge_requests/1256
    
    Fixes: https://github.com/fedora-modularity/libmodulemd/pull/414
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/include/private/glib-extensions.h
+++ modulemd/include/private/glib-extensions.h
@@ -11,6 +11,13 @@
  * For more information on free software, see <https://www.gnu.org/philosophy/free-sw.en.html>.
  */
 
+#pragma once
+
 #include <glib.h>
 
-G_DEFINE_AUTOPTR_CLEANUP_FUNC (GDate, g_date_free);
+#include "config.h"
+
+/* GDate autoptr cleanup was finally added in GLib 2.63.3. */
+#ifndef HAVE_GDATE_AUTOPTR
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (GDate, g_date_free)
+#endif

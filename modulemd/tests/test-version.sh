edf69363ed988ddf422124b4e6b4b693114e1242
Stephen Gallagher <sgallagh@redhat.com>
2020-02-11 14:48:28 -0500

    Add version check test
    This test will ensure that we remember to bump the minor release
    version after making a release. This will help avoid issues in the
    CI infrastructure where the installed-library tests are running
    against a downstream release instead of testing the changed code.
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

---
+++ modulemd/tests/test-version.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $MESON_BUILD_ROOT
+
+function error_out {
+    local code message
+    local "${@}"
+
+    echo $message 1>&2
+    exit $code
+}
+
+function common_finalize {
+    exitcode=$?
+
+    return $exitcode
+}
+
+trap common_finalize EXIT
+
+jq --version >/dev/null || error_out code=127 message="Install 'jq' to use this script"
+
+# Get the previous tag for this branch
+OLDTAG=$(git describe --first-parent --abbrev=0)
+
+# Get the version that will be tagged
+NEWVERSION=$(meson introspect $MESON_BUILD_ROOT --projectinfo |jq -r .version)
+NEWTAG=libmodulemd-$NEWVERSION
+
+if [ $NEWTAG = $OLDTAG ]; then
+    error_out code=2 message="Version is already tagged. Update meson.build with the new version."
+fi
+

c09e99d7e6885b751d31acdb68047cb89e8b2857
Stephen Gallagher <sgallagh@redhat.com>
2020-02-12 09:36:10 -0500

    Rework specfile template
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- spec_tmpl.sh
+++ spec_tmpl.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-version=$1
-template=$2
-datetime=$(date +%Y%m%d%H%M%S)
+function main {
+    local template version release
+    local "${@}"
 
-sed -e "s/@VERSION@/$version/" \
-    -e "s/@DATETIME@/$datetime/" $template
+    datetime=$(date +%Y%m%d%H%M%S)
+    release=${release:-0.$datetime%\{?dist\}}
+
+    sed -e "s/@VERSION@/$version/" \
+        -e "s/@RELEASE@/$release/" $template
+}
+
+main "${@}"

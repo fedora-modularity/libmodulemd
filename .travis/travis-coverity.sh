6629c3610a52fb131dcdb879ae9b8ce86cedb547
Merlin Mathesius <mmathesi@redhat.com>
2020-09-23 14:15:46 -0500

    Enhance the travis build scripts and Dockerfile templates so that the common mmd_run_docker_tests() launcher function can also launch the Coverity scan. This enhancement made it a simple matter to switch the Coverity scan to run on CentOS 8 instead of Fedora.
    The travis document generation was also able to take advantage of this
    enhancement.
    
    Signed-off-by: Merlin Mathesius <mmathesi@redhat.com>

--- .travis/travis-coverity.sh
+++ .travis/travis-coverity.sh
@@ -8,58 +8,21 @@ source $SCRIPT_DIR/travis-common.inc
 set -e
 set -x
 
-function coverity_finalize {
-    exitcode=$?
+os=centos
+release=8
+repository=docker.io
 
-    # Make sure to delete the Dockerfile.deps from fedora
-    rm -f $SCRIPT_DIR/$MMD_OS/Dockerfile.deps.$MMD_RELEASE
+# Override the standard tests with the Coverity scan
+mmd_run_docker_tests \
+    os=$os \
+    release=$release \
+    repository=$repository \
+    test_template="coverity/Dockerfile.tmpl" \
+    test_image="libmodulemd-coverity" \
+    oci_extra_args="
+        -e COVERITY_SCAN_TOKEN=$COVERITY_SCAN_TOKEN
+        -e TRAVIS=$TRAVIS
+        -e TRAVIS_COMMIT='$TRAVIS_COMMIT'
+    "
 
-    common_finalize
-
-    return $exitcode
-}
-
-trap coverity_finalize EXIT
-
-# Always run the Coverity scan on the oldest supported Fedora
-# because it generally lags behind with GCC compatibility.
-MMD_OS=fedora
-MMD_RELEASE=31
-MMD_IMAGE=fedora/fedora:${MMD_RELEASE}-$(uname -m)
-repository="quay.io"
-
-# Create an archive of the current checkout
-MMD_TARBALL_PATH=`mktemp -p $SCRIPT_DIR tarball-XXXXXX.tar.bz2`
-TARBALL=`basename $MMD_TARBALL_PATH`
-
-pushd $SCRIPT_DIR/..
-git ls-files |xargs tar cfj $MMD_TARBALL_PATH .git
-popd
-
-sed -e "s#@IMAGE@#$repository/${MMD_IMAGE}#" \
-    $SCRIPT_DIR/fedora/Dockerfile.deps.tmpl > $SCRIPT_DIR/coverity/Dockerfile.deps.$MMD_RELEASE
-
-sed -e "s#@RELEASE@#${MMD_RELEASE}#" $SCRIPT_DIR/coverity/Dockerfile.tmpl \
-    | m4 -D_RELEASE_=$release \
-    > $SCRIPT_DIR/coverity/Dockerfile-$MMD_RELEASE
-
-$RETRY_CMD $MMD_BUILDAH $MMD_LAYERS_TRUE \
-    -f $SCRIPT_DIR/coverity/Dockerfile.deps.$MMD_RELEASE \
-    -t fedora-modularity/libmodulemd-deps-$MMD_OS:$MMD_RELEASE .
-
-$RETRY_CMD $MMD_BUILDAH $MMD_LAYERS_FALSE \
-    -f $SCRIPT_DIR/coverity/Dockerfile-$MMD_RELEASE \
-    -t fedora-modularity/libmodulemd-coverity \
-    --build-arg TARBALL=$TARBALL .
-
-rm -f $MMD_TARBALL_PATH $SCRIPT_DIR/fedora/Dockerfile.deps.$MMD_RELEASE $SCRIPT_DIR/fedora/Dockerfile-$MMD_RELEASE
-
-# Override the standard tasks with the Coverity scan
-$RETRY_CMD $MMD_OCI run \
-    -e COVERITY_SCAN_TOKEN=$COVERITY_SCAN_TOKEN \
-    -e TRAVIS=$TRAVIS \
-    -e TRAVIS_COMMIT="$TRAVIS_COMMIT" \
-    --rm fedora-modularity/libmodulemd-coverity
-
-popd
-
+popd # $SCRIPT_DIR

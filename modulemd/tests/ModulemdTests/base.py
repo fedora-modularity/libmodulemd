3a65ddcde54fb68afe879c8668fcfa29f268cf8d
Stephen Gallagher <sgallagh@redhat.com>
2022-01-11 09:13:04 -0500

    Handle assertRaisesRegex() on older python
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/tests/ModulemdTests/base.py
+++ modulemd/tests/ModulemdTests/base.py
@@ -56,3 +56,26 @@ class TestBase(unittest.TestCase):
     def warnings_fatal(self):
         gdebug = os.getenv("G_DEBUG", "").split(",")
         return "fatal-warnings" in gdebug
+
+    def assertRaisesRegex(
+        self, expected_exception, expected_regex, *args, **kwargs
+    ):
+        """Asserts that the message in a raised exception matches a regex.
+
+        Args:
+            expected_exception: Exception class expected to be raised.
+            expected_regex: Regex (re.Pattern object or string) expected
+                    to be found in error message.
+            args: Function to be called and extra positional args.
+            kwargs: Extra kwargs.
+            msg: Optional message used in case of failure. Can only be used
+                    when assertRaisesRegex is used as a context manager.
+        """
+        try:
+            return super().assertRaisesRegex(
+                expected_exception, expected_regex, *args, **kwargs
+            )
+        except AttributeError:
+            return super().assertRaisesRegexp(
+                expected_exception, expected_regex, *args, **kwargs
+            )
